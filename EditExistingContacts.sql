488fbd065bff77050d7eac8c23955bf59313ae13
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-30 08:58:41 +0530

    edit existing Contact Person using their Name

---
+++ EditExistingContacts.sql
@@ -0,0 +1,4 @@
+update Address_Book_Table set EmailId='harsh32@gmail.com' where FirstName='Harish';
+update Address_Book_Table set Address='Mambakkam' where FirstName='Kishore';
+update Address_Book_Table set Phonenum=7390802345 where FirstName='Swetha';
+select * from Address_Book_Table;
