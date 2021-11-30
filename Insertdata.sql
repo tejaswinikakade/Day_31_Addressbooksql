d62eba3ad26b018fb4229ad7108836f397ac72ef
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-30 08:47:43 +0530

    Creating a Table in Database

---
+++ Insertdata.sql
@@ -0,0 +1,4 @@
+Insert into Address_Book_Table 
+values('Harish','Krishnan','Jew Town','Cochin','Kerala',60035,8657,'harsh@gmail.com'),
+('Kishore','Kumar','ECR','Chennai','TamilNadu',60025,9845,'kishore2000@gmail.com'),
+('Swetha','Raju','P.N.Pudhur','Coimbatore','TamilNadu',600082,8967,'swetha123@gmail.com');
