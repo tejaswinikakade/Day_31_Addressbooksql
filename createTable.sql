1f3f26535f2d831ad0bcd7d533dde9e9ea4b792a
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-30 08:35:40 +0530

    Creating a Table in Database

---
+++ createTable.sql
@@ -0,0 +1,11 @@
+create table Address_Book_Table
+(
+FirstName varchar(50) not null,
+LastName varchar(20),
+Address varchar(100),
+City varchar(50),
+StateName varchar(55),
+ZipCode int,
+Phonenum int,
+EmailId varchar(100),
+PRIMARY KEY (`Phonenum`));
